0b1e1253e78a4c717fc40a1be210151d3dcb31b7
navi-04 <naveenrajthiyagarajan6@gmail.com>
2025-09-12 22:31:13 +0530

    added all the images to the flavour section

--- data.js
+++ data.js
@@ -2,47 +2,181 @@
 const flavours = [
     {
         id: 1,
-        name: "Strawberry Splash",
-        description: "Sweet, juicy strawberries picked at peak ripeness",
+        name: "Mango",
+        description: "Refreshing mango purée with a tropical zing",
         image: "./images/1.JPG",
-        tags: ["Bestseller", "Vegan"]
+        tags: ["Bestseller", "Gluten-free"]
     },
     {
         id: 2,
-        name: "Mango Tango",
-        description: "Tropical mango purée with a hint of lime",
+        name: "Tender Coconut",
+        description: "Smooth coconut water with tender coconut flesh",
         image: "./images/2.JPG",
-        tags: ["New", "Gluten-free"]
+        tags: ["Vegan", "Dairy-free"]
     },
     {
         id: 3,
-        name: "Blueberry Bliss",
-        description: "Wild blueberries with a touch of honey",
-        image: "popsicle.jpg",
-        tags: ["Seasonal"]
+        name: "Lime Mint Mojito",
+        description: "Zesty lime and refreshing mint blend for a mojito twist",
+        image: "./images/3.JPG",
+        tags: ["New", "Gluten-free"]
     },
     {
         id: 4,
-        name: "Creamy Coconut",
-        description: "Rich coconut cream with vanilla bean",
-        image: "popsicle.jpg",
-        tags: ["Dairy-free"]
+        name: "Avocado",
+        description: "Creamy avocado with a hint of sweetness",
+        image: "./images/4.JPG",
+        tags: ["Healthy", "Vegan"]
     },
     {
         id: 5,
-        name: "Chocolate Dream",
-        description: "Rich dark chocolate with a velvety texture",
-        image: "popsicle.jpg",
-        tags: ["Classic"]
+        name: "Blue Berry",
+        description: "Fresh wild blueberries with a hint of tang",
+        image: "./images/5.JPG",
+        tags: ["Seasonal", "Antioxidant-rich"]
     },
-        {
+    {
         id: 6,
-        name: "Strawberry Splash",
-        description: "Sweet, juicy strawberries picked at peak ripeness",
-        image: "popsicle.jpg",
+        name: "Strawberry",
+        description: "Sweet strawberries picked at peak ripeness",
+        image: "./images/6.JPG",
         tags: ["Bestseller", "Vegan"]
     },
+    {
+        id: 7,
+        name: "Blue Carraco",
+        description: "Exotic blend of blue carraco with tropical hints",
+        image: "./images/7.JPG",
+        tags: ["Unique", "Limited Edition"]
+    },
+    {
+        id: 8,
+        name: "Jackfruit",
+        description: "Rich, tropical jackfruit chunks in a creamy base",
+        image: "./images/8.JPG",
+        tags: ["Seasonal", "Vegan"]
+    },
+    {
+        id: 9,
+        name: "Custard Apple",
+        description: "Tangy apple flavor with a custard-like creaminess",
+        image: "./images/9.JPG",
+        tags: ["Classic", "New"]
+    },
+    {
+        id: 10,
+        name: "Pista",
+        description: "Nutty pistachio flavor with a creamy texture",
+        image: "./images/10.JPG",
+        tags: ["Popular", "Nutty"]
+    },
+    {
+        id: 11,
+        name: "Gulab Jam",
+        description: "Traditional Indian dessert flavor with syrup-soaked gulab jamuns",
+        image: "./images/11.JPG",
+        tags: ["Dessert", "Rich"]
+    },
+    {
+        id: 12,
+        name: "Coffee",
+        description: "Bold coffee flavor with a velvety finish",
+        image: "./images/12.JPG",
+        tags: ["Classic", "Caffeine"]
+    },
+    {
+        id: 13,
+        name: "Litchi",
+        description: "Sweet litchi flavor with floral notes",
+        image: "./images/13.JPG",
+        tags: ["Fruity", "Exotic"]
+    },
+    {
+        id: 14,
+        name: "Chocolate",
+        description: "Rich dark chocolate with a velvety texture",
+        image: "./images/14.JPG",
+        tags: ["Classic", "Popular"]
+    },
+    {
+        id: 15,
+        name: "Malai",
+        description: "Rich cream-based malai flavor for smooth indulgence",
+        image: "./images/15.JPG",
+        tags: ["Creamy", "Classic"]
+    },
+    {
+        id: 16,
+        name: "Lotus Biscoff",
+        description: "Lotus biscuit flavor blended with cream for crunch and sweetness",
+        image: "./images/16.JPG",
+        tags: ["Unique", "Dessert"]
+    },
+    {
+        id: 17,
+        name: "Spanish Delight",
+        description: "Exotic Spanish flavor with sweet and aromatic hints",
+        image: "./images/17.JPG",
+        tags: ["Limited Edition", "New"]
+    },
+    {
+        id: 18,
+        name: "Milo",
+        description: "Chocolate malt flavor inspired by the classic Milo drink",
+        image: "./images/18.JPG",
+        tags: ["Classic", "Energy Boost"]
+    },
+    {
+        id: 19,
+        name: "Grape",
+        description: "Juicy grape flavor with a touch of tartness",
+        image: "./images/19.JPG",
+        tags: ["Fruity", "Refreshing"]
+    },
+    {
+        id: 20,
+        name: "Sapota",
+        description: "Sweet sapota fruit flavor blended into creamy texture",
+        image: "./images/20.JPG",
+        tags: ["Exotic", "Seasonal"]
+    },
+    {
+        id: 21,
+        name: "Pingo",
+        description: "A tropical surprise mix with sweet and tangy pingo flavor",
+        image: "./images/21.JPG",
+        tags: ["Unique", "Limited"]
+    },
+    {
+        id: 22,
+        name: "Orange",
+        description: "Zesty orange flavor, refreshingly citrusy",
+        image: "./images/22.JPG",
+        tags: ["Citrus", "Vitamin C"]
+    },
+    {
+        id: 23,
+        name: "Butterscotch",
+        description: "Rich butterscotch flavor with caramel notes",
+        image: "./images/23.JPG",
+        tags: ["Classic", "Dessert"]
+    },
+    {
+        id: 24,
+        name: "Kitkat",
+        description: "Chocolate and wafer crunch inspired by Kitkat",
+        image: "./images/24.JPG",
+        tags: ["Chocolate", "Crunchy"]
+    },
+    {
+        id: 25,
+        name: "Watermelon",
+        description: "Fresh watermelon flavor, juicy and sweet",
+        image: "./images/25.JPG",
+        tags: ["Fruity", "Refreshing"]
+    },
 ];
+
 
 // About section feature data
 const aboutFeatures = [
